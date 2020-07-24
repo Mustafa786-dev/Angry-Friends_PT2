862e87b7d38a1d54555f85e6bc485c468d347e79
Mustafa786-dev <63359849+Mustafa786-dev@users.noreply.github.com>
2020-07-24 13:55:07 +0530

    Update Sierra.js

--- Sierra.js
+++ Sierra.js
@@ -1,7 +1,7 @@
 class Sierra extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
-    this.image = loadImage("sprites/sierra.png",100,100);
+    this.image = loadImage("sierra.png",100,100);
     this.Visiblity = 255;
   }
 
